d8e08b6e16a08371fa4edb0c83f727f6468b813d
sweyome <sweyome1@gmail.com>
2020-06-30 22:30:58 -0400

    BE done

--- js/index.js
+++ js/index.js
@@ -3,18 +3,17 @@ const url = "https://poerfolio-api.herokuapp.com/api/contacts"
 
 
 const sendHttpRequest = (method, url,data) =>{
-    return fetch(url).then(response =>{
+    return fetch(url, {
+        method: method,
+        body: JSON.stringify(data),
+        headers: data ? {'content-Type': 'application/json' } :{}
+    }).then(response =>{
         return response.json();
-    })
-    .then(responseData =>{
-        console.log(responseData)
-    })
-}
+    });
+};
 
 const getData = () => {
-    fetch(url).then(response =>{
-        return response.json();
-    })
+    sendHttpRequest('GETz', url)
     .then(responseData =>{
         console.log(responseData)
     })
@@ -22,12 +21,13 @@ const getData = () => {
 
 
 const sendData = () => {
-    fetch(url).then(response =>{
-        return response.json();
-    })
-    .then(responseData =>{
-        console.log(responseData)
+    sendHttpRequest('POST', url, {
+    "name": "dfeffff",
+    "email": "rowValue21@gmail.com",
+    "message": "blablabalkabaka"
+    }).then(responseData =>{
+        console.log(responseData);
     })
 }
 
-sendBtn.addEventListener('click', getData)
+sendBtn.addEventListener('click', sendData)
