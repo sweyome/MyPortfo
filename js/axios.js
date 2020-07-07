da2765c33f646cf8ec6de8f9b6276203d16b9fa0
sweyome <sweyome1@gmail.com>
2020-07-06 21:44:14 -0400

    BE axios done

---
+++ js/axios.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById("sMessage");
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+
+const nameValue = () =>{
+    const name = document.getElementById('name').value;
+    return name
+}
+const emailValue = () =>{
+    const email = document.getElementById('email').value;
+    return email
+}
+const messageValue = () =>{
+    const message = document.getElementById('message').value;
+    return message
+}
+
+
+const sendData = () => {
+    axios.post('https://poerfolio-api.herokuapp.com/api/contacts', {
+        name: nameValue(),
+        email: emailValue(),
+        message: messageValue() 
+    }).then(res => {
+        console.log(res)
+    })
+}
+
+btn.addEventListener('click', sendData);
+console.log(btn)
